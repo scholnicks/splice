86ac16e8ac26e366ae3b3f395fe66ec23082d45f
scholnicks <steve@scholnick.net>
2014-01-25 10:16:00 -0500

    backed out changes to use eclipse/pydev

---
+++ splice/label.py
@@ -0,0 +1,14 @@
+
+class Label(object):
+    def __init__(self):
+        pass    
+    
+class TapeLabel(Label):
+    pass
+
+class CDLabel(Label):
+    pass
+
+class LabelFactory(object):
+    pass
+
